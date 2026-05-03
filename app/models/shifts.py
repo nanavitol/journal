908d9465e3d0362c6d645fa6565ddcdd9cf125c2
nanavitol <vodalchuk4@gmail.com>
2026-05-03 18:22:25 +0700

    fix status of shifts!

--- app/models/shifts.py
+++ app/models/shifts.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, Date, DateTime, func, ForeignKey, Enum, UniqueConstraint
+from sqlalchemy import Column, BigInteger, Date, DateTime, func, ForeignKey, UniqueConstraint, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from app.core.database import Base
